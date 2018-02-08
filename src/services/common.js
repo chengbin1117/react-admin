685a93777480486a2fba2920370cbfb188bb3d0f
chengbin <chengbin@kg.com>
2018-02-08 13:18:44 +0800

    'fix'

--- src/services/common.js
+++ src/services/common.js
@@ -150,10 +150,10 @@ let ImgUrl = "https://www.kg.com/image/upload"; //生产&&测试
 
 //下载图片
 
-let uploadUrl = "https://kgcom.oss-cn-shenzhen.aliyuncs.com/";//开发
+// let uploadUrl = "https://kgcom.oss-cn-shenzhen.aliyuncs.com/";//开发
 
 //let uploadUrl = "https://kgtest01.oss-cn-beijing.aliyuncs.com/"; //测试
-// let uploadUrl = "https://pro-kg-oss.oss-cn-beijing.aliyuncs.com/"; //生产
+let uploadUrl = "https://pro-kg-oss.oss-cn-beijing.aliyuncs.com/"; //生产
 
 //服务器
 //let urlprefix = "http://172.16.1.108:8081/kgapi";  //李熠
