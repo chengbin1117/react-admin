aea438a4e4841b04ba46c80d1ac5c4b1b3b132ba
郭康 <guokang@kg.com>
2018-04-13 10:25:30 +0800

    优化代码

--- src/services/common.js
+++ src/services/common.js
@@ -238,9 +238,9 @@ let apkUrl = "http://172.16.0.243:8080/kgapi/admin/appvm/uploadFile"; //开发�
 
 //服务器
 //let urlprefix = "http://172.16.1.108:8081/kgapi";  //李熠
-let urlprefix = "http://172.16.1.97:8080/kgapi";  //文全
+//let urlprefix = "http://172.16.1.97:8080/kgapi";  //文全
 //let urlprefix = "http://172.16.0.243:8080/kgapi";  //王洋
-//let urlprefix = "http://kg.btc123.com/kgapi";//开发
+let urlprefix = "http://kg.btc123.com/kgapi";//开发
 //let urlprefix = "https://www.kg.com"; //生产&&测试
 
 
