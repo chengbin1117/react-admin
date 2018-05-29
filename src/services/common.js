c91d7181cd2f93a95e3e9c6175d24401ea81b54a
郭康 <guokang@kg.com>
2018-05-29 17:42:22 +0800

    修改冲突

--- src/services/common.js
+++ src/services/common.js
@@ -217,14 +217,14 @@ export function  getBase64(img){
       }
 
 //预览前台地址
-let previewUrl = "http://kg.btc123.com/dist/views/detail/list.html"; //开发
-//let previewUrl = "https://kg.com/detail/list.html"; //测试
+//let previewUrl = "http://kg.btc123.com/dist/views/detail/list.html"; //开发
+let previewUrl = "https://kg.com/detail/list.html"; //测试
 
 
 
 
-let ImgUrl = "http://kg.btc123.com/kgapi/image/upload"; //开发
-//let ImgUrl = "https://www.kg.com/image/upload"; //生产&&测试
+//let ImgUrl = "http://kg.btc123.com/kgapi/image/upload"; //开发
+let ImgUrl = "https://www.kg.com/image/upload"; //生产&&测试
 //
 //下载图片
 let uploadUrl = "https://kgcom.oss-cn-shenzhen.aliyuncs.com/";//开发
@@ -233,22 +233,22 @@ let uploadUrl = "https://kgcom.oss-cn-shenzhen.aliyuncs.com/";//开发
 
 
 //上传视频地址
-let uploadVideoUrl = "http://kg.btc123.com/kgapi/image/uploadVideo"; //开发环境地址
-//let uploadVideoUrl = "https://www.kg.com/image/uploadVideo"; //测试&&正式环境地址
+//let uploadVideoUrl = "http://kg.btc123.com/kgapi/image/uploadVideo"; //开发环境地址
+let uploadVideoUrl = "https://www.kg.com/image/uploadVideo"; //测试&&正式环境地址
 //下载视频地址
 let videoUrl = "https://kgvideo.oss-cn-shenzhen.aliyuncs.com/"; //开发环境视频服务器地址
 //let videoUrl = "https://pro-kg-video-oss.oss-cn-beijing.aliyuncs.com/"; //正式环境视频服务器地址
 
 //apk上传
-let apkUrl = "http://172.16.0.243:8080/kgapi/admin/appvm/uploadFile"; //开发环境视频服务器地址
-//let apkUrl = "https://www.kg.com/admin/appvm/uploadFile/"; //正式环境视频服务器地址
+//let apkUrl = "http://172.16.0.243:8080/kgapi/admin/appvm/uploadFile"; //开发环境视频服务器地址
+let apkUrl = "https://www.kg.com/admin/appvm/uploadFile/"; //正式环境视频服务器地址
 
 //服务器
 //let urlprefix = "http://172.16.1.108:8081/kgapi";  //李熠
 //let urlprefix = "http://172.16.1.97:8080/kgapi";  //文全
 //let urlprefix = "http://172.16.0.243:8080/kgapi";  //王洋
-let urlprefix = "http://kg.btc123.com/kgapi";//开发
-//let urlprefix = "https://www.kg.com"; //生产&&测试
+//let urlprefix = "http://kg.btc123.com/kgapi";//开发
+let urlprefix = "https://www.kg.com"; //生产&&测试
 
 
 
