a766742c07e71b7a33637354f8452d8808fcef30
郭康 <guokang@kg.com>
2018-06-01 16:07:28 +0800

    修改dev环境配置

--- src/services/common.js
+++ src/services/common.js
@@ -239,13 +239,13 @@ export function getBase64(img) {
 }
 
 //预览前台地址
-let previewUrl = "http://kg.btc123.com/dist/views/detail/list.html"; //开发
+let previewUrl = "https://dev.kg.com/dist/views/detail/list.html"; //开发
 //let previewUrl = "https://kg.com/detail/list.html"; //测试
 
 
 
 
-let ImgUrl = "http://kg.btc123.com/kgapi/image/upload"; //开发
+let ImgUrl = "https://dev.kg.com/image/upload"; //开发
 //let ImgUrl = "https://www.kg.com/image/upload"; //生产&&测试
 //
 //下载图片
@@ -255,22 +255,22 @@ let uploadUrl = "https://kgcom.oss-cn-shenzhen.aliyuncs.com/";//开发
 
 
 //上传视频地址
-let uploadVideoUrl = "http://kg.btc123.com/kgapi/image/uploadVideo"; //开发环境地址
+let uploadVideoUrl = "https://dev.kg.com/image/uploadVideo"; //开发环境地址
 //let uploadVideoUrl = "https://www.kg.com/image/uploadVideo"; //测试&&正式环境地址
 //下载视频地址
 let videoUrl = "https://kgvideo.oss-cn-shenzhen.aliyuncs.com/"; //开发环境视频服务器地址
 //let videoUrl = "https://pro-kg-video-oss.oss-cn-beijing.aliyuncs.com/"; //正式环境视频服务器地址
 
 //apk上传
-let apkUrl = "http://172.16.0.243:8080/kgapi/admin/appvm/uploadFile"; //开发环境视频服务器地址
+let apkUrl = "https://dev.kg.com/admin/appvm/uploadFile"; //开发环境视频服务器地址
 //let apkUrl = "https://www.kg.com/admin/appvm/uploadFile/"; //正式环境视频服务器地址
 
 //服务器
 //let urlprefix = "http://172.16.1.108:8081/kgapi";  //李熠
 //let urlprefix = "http://172.16.1.97:8080/kgapi";  //文全
 //let urlprefix = "http://172.16.0.243:8080/kgapi";  //王洋
-let urlprefix = "http://172.16.2.62:8083/";  //连鹏
-//let urlprefix = "http://kg.btc123.com/kgapi";//开发
+//let urlprefix = "http://172.16.2.62:8083/";  //连鹏
+let urlprefix = "https://dev.kg.com";//开发
 //let urlprefix = "https://www.kg.com"; //生产&&测试
 
 
